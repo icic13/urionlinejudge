3b7409f20cdfd998ef6e8ef602ff82644cf3c164
Md. Rana Mahmud <ranamd0@gmail.com>
2016-03-28 01:26:01 +0600

    URI 1566 Solved

---
+++ URI Online Judge | 1566.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int nc;
+    int n;
+    cin>>nc;
+    while(nc-->0)
+    {
+       scanf("%d",&n);
+        vector<int> people(n);
+        for(int i = 0; i<n; i++)
+            scanf("%d",&people[i]);
+        sort(people.begin(),people.end());
+
+        for(int i = 0; i<n-1; i++)
+            printf("%d ",people[i]);
+
+        printf("%d",people[n-1]);
+        printf("\n");
+
+    }
+
+
+}
