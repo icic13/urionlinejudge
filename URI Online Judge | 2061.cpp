9d04082a7cc545fb8c1d034fae7c1c46ec529fed
Md. Rana Mahmud <ranamd0@gmail.com>
2016-05-27 04:00:24 +0600

    uri 1796 solved

--- URI Online Judge | 2061.cpp
+++ URI Online Judge | 2061.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    int counter,N,M,close = 0,
+    clicked = 0;
+    string action;
+    cin>>N>>M;
+    for(counter  = 0; counter<5; counter++){
+            cin>>action;
+        if(action=="fechou")
+            close++;
+        if(action=="clicou")
+            clicked++;
+
+
+
+    }
+
+    cout<<N-close-clicked+close*2<<endl;
+
+
+
+
+}
